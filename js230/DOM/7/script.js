3a7ba7e27ee9e8aa185b76c334800a8aa90b4669
William Baker <wbaker@gmail.com>
2020-05-20 13:42:25 -0400

    re-organized

--- js230/DOM/7/script.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener('DOMContentLoaded', () => {
-  function nodesToArr(node = document.body) {
-    let otherStuff = [];
-    [].forEach.call(node.children, (elem) => otherStuff.push(nodesToArr(elem)));
-    return [node.tagName, otherStuff];
-  }
-
-  console.log(nodesToArr());
-});
